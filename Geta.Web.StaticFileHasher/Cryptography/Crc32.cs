347005481de24b911b36eea85e64b55024f91ddf
Sven-Erik Jonsson <sven.erik@geta.no>
2016-06-23 09:56:41 +0200

    Initial commit

---
+++ Geta.Web.StaticFileHasher/Cryptography/Crc32.cs
@@ -0,0 +1,154 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+
+namespace Geta.Web.StaticFileHasher.Cryptography
+{
+    public class Crc32 : HashAlgorithm
+    {
+        public const uint DefaultPolynomial = 0xedb88320;
+        public const uint DefaultSeed = 0xffffffff;
+        public const uint BufferSize = 4096;
+
+        private uint hash;
+        private readonly uint seed;
+        private readonly uint[] table;
+        private static uint[] defaultTable;
+
+        public Crc32()
+        {
+            table = InitializeTable(DefaultPolynomial);
+            seed = DefaultSeed;
+            Initialize();
+        }
+
+        public Crc32(uint polynomial, uint seed)
+        {
+            table = InitializeTable(polynomial);
+            this.seed = seed;
+            Initialize();
+        }
+
+        public override sealed void Initialize()
+        {
+            hash = seed;
+        }
+
+        protected override void HashCore(byte[] buffer, int start, int length)
+        {
+            hash = CalculateHash(table, hash, buffer, start, length);
+        }
+
+        protected override byte[] HashFinal()
+        {
+            var hashBuffer = UInt32ToBigEndianBytes(~hash);
+            HashValue = hashBuffer;
+            return hashBuffer;
+        }
+
+        public override int HashSize
+        {
+            get { return 32; }
+        }
+
+        public static uint Compute(Stream stream)
+        {
+            return ~Compute(InitializeTable(DefaultPolynomial), DefaultSeed, stream);
+        }
+
+        public static uint Compute(uint[] table, uint seed, Stream stream)
+        {
+            var buffer = new byte[BufferSize];
+            int read;
+
+            do
+            {
+                read = stream.Read(buffer, 0, buffer.Length);
+                seed = ~CalculateHash(table, seed, buffer, 0, read);
+            }
+            while (read > 0);
+
+            return seed;
+        }
+
+        public static uint Compute(byte[] buffer)
+        {
+            return ~CalculateHash(InitializeTable(DefaultPolynomial), DefaultSeed, buffer, 0, buffer.Length);
+        }
+
+        public static uint Compute(uint[] table, byte[] buffer)
+        {
+            return ~CalculateHash(table, DefaultSeed, buffer, 0, buffer.Length);
+        }
+
+        public static uint Compute(uint seed, byte[] buffer)
+        {
+            return ~CalculateHash(InitializeTable(DefaultPolynomial), seed, buffer, 0, buffer.Length);
+        }
+
+        public static uint Compute(uint polynomial, uint seed, byte[] buffer)
+        {
+            return ~CalculateHash(InitializeTable(polynomial), seed, buffer, 0, buffer.Length);
+        }
+
+        public static uint Compute(uint[] table, uint seed, byte[] buffer)
+        {
+            return ~CalculateHash(table, seed, buffer, 0, buffer.Length);
+        }
+
+        public static uint[] InitializeTable(uint polynomial)
+        {
+            if (polynomial == DefaultPolynomial && defaultTable != null)
+                return defaultTable;
+
+            var createTable = new uint[256];
+            for (var i = 0u; i < 256; i++)
+            {
+                var entry = i;
+
+                for (var j = 0; j < 8; j++)
+                {
+                    if ((entry & 1) == 1)
+                    {
+                        entry = (entry >> 1) ^ polynomial;
+                    }
+                    else
+                    {
+                        entry = entry >> 1;
+                    }
+                }
+
+                createTable[i] = entry;
+            }
+
+            if (polynomial == DefaultPolynomial)
+                defaultTable = createTable;
+
+            return createTable;
+        }
+
+        private static uint CalculateHash(uint[] table, uint seed, byte[] buffer, int start, int size)
+        {
+            var crc = seed;
+            for (var i = start; i < size; i++)
+            {
+                unchecked
+                {
+                    crc = (crc >> 8) ^ table[buffer[i] ^ crc & 0xff];
+                }
+            }
+
+            return crc;
+        }
+
+        protected byte[] UInt32ToBigEndianBytes(uint x)
+        {
+            return new[]
+            {
+                (byte)((x >> 24) & 0xff),
+                (byte)((x >> 16) & 0xff),
+                (byte)((x >> 8) & 0xff),
+                (byte)(x & 0xff)
+            };
+        }
+    }
+}
---
+++ Geta.Web.StaticFileHasher/Helpers/FileHashHelper.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.WebPages.Html;
+using Geta.Web.StaticFileHasher.Cryptography;
+
+namespace Geta.Web.StaticFileHasher.Helpers
+{
+    public static class FileHashHelper
+    {
+        private static readonly char[] _relativePathIndicators = { '~', '/', '.' };
+
+        private static readonly IEqualityComparer<string> _comparer = StringComparer.InvariantCultureIgnoreCase;
+        private static readonly IDictionary<string, string> _files = new Dictionary<string, string>(_comparer);
+        private static readonly IDictionary<string, object> _locks = new Dictionary<string, object>(_comparer);
+        private static readonly IDictionary<string, FileSystemWatcher> _watchers = new Dictionary<string, FileSystemWatcher>(_comparer);
+
+        private static readonly uint[] _table = Crc32.InitializeTable(Crc32.DefaultPolynomial);
+        private static readonly uint _seed = Crc32.DefaultSeed;
+        private static readonly object _masterLock = new object();
+
+        public static string GetHash(string file)
+        {
+            file = ResolvePath(file);
+            if (string.IsNullOrEmpty(file)) return file;
+
+            AppendLock(file);
+
+            lock (_locks[file])
+            {
+                if (_files.ContainsKey(file)) return _files[file];
+
+                var hash = ComputeHash(file);
+                if (string.IsNullOrEmpty(hash)) return file;
+
+                _files.Add(file, hash);
+
+                var folder = Path.GetDirectoryName(file);
+                if (!_watchers.ContainsKey(folder))
+                {
+                    _watchers.Add(folder, CreateWatcher(folder));
+                }
+
+                return hash;
+            }
+        }
+
+        private static void AppendLock(string file)
+        {
+            lock (_masterLock)
+            {
+                if (!_locks.ContainsKey(file))
+                    _locks.Add(file, new object());
+            }
+        }
+
+        public static IHtmlString AppendHash(this HtmlHelper helper, string url)
+        {
+            var hash = GetHash(url);
+            if (string.IsNullOrEmpty(hash)) return new HtmlString(url);
+
+            url = url.TrimStart('~');
+
+            var index = url.IndexOf('?');
+            var symbol = index > -1 ? "&" : "?";
+
+            return new HtmlString(url + symbol + hash);
+        }
+
+        private static FileSystemWatcher CreateWatcher(string folder)
+        {
+            var watcher = new FileSystemWatcher(folder)
+            {
+                Path = folder,
+                IncludeSubdirectories = false,
+                NotifyFilter = NotifyFilters.LastWrite,
+                EnableRaisingEvents = true
+            };
+
+            watcher.Changed += OnFileChange;
+            watcher.Renamed += OnFileRenamed;
+            watcher.Deleted += OnFileDeleted;
+
+            return watcher;
+        }
+
+        private static void OnFileDeleted(object sender, FileSystemEventArgs e)
+        {
+            if (_files.ContainsKey(e.FullPath)) _files.Remove(e.FullPath);
+        }
+
+        private static void OnFileRenamed(object sender, RenamedEventArgs e)
+        {
+            if (_files.ContainsKey(e.OldFullPath)) _files.Remove(e.OldFullPath);
+        }
+
+        private static void OnFileChange(object sender, FileSystemEventArgs e)
+        {
+            if (_files.ContainsKey(e.FullPath)) _files.Remove(e.FullPath);
+        }
+
+        private static string ComputeHash(string file)
+        {
+            uint hash;
+
+            try
+            {
+                using (var stream = new FileStream(file, FileMode.Open, FileAccess.Read))
+                {
+                    hash = Crc32.Compute(_table, _seed, stream);
+                }
+            }
+            catch (FileLoadException)
+            {
+                return null;
+            }
+
+            return HttpServerUtility.UrlTokenEncode(BitConverter.GetBytes(hash));
+        }
+
+        private static string ResolvePath(string file)
+        {
+            if (file.IndexOfAny(_relativePathIndicators) == 0)
+                file = HostingEnvironment.MapPath(file);
+
+            return !File.Exists(file) ? string.Empty : file;
+        }
+    }
+}
